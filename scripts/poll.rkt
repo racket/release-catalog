787acdd2c04ba63104729e5025f7ea7b9780b9b0
John Clements <clements@racket-lang.org>
2025-02-27 16:22:27 -0800

    Revert "update catalog" accidental commit
    This reverts commit 5040a7581865368013ce6f9979ff2c6994cfe151.

--- scripts/poll.rkt
+++ scripts/poll.rkt
@@ -41,8 +41,6 @@
 ;; poll-pkg : InfoHash Cache -> InfoHash/#f
 (define (poll-pkg info cache)
   (define source (hash-ref info 'source))
-    (printf "source: ~e\n" source)
-
   (define old-checksum (hash-ref info 'checksum))
   (match (parse-repo-url source)
     [(list user repo old-branch path)
